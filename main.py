68d13c405bd18ab867d6d6e86aea6340fd793391
martsalazar <martsalazar0@gmail.com>
2025-07-26 22:08:39 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import socket
+
+def scan_ports(target, start_port=1, end_port=1024, timeout=1.0):
+    print(f"Scanning target {target} from port {start_port} to {end_port}...\n")
+    open_ports = []
+
+    for port in range(start_port, end_port + 1):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(timeout)
+
+            result = sock.connect_ex((target, port)) # 0 means port open
+            if(result == 0):
+                print(f"[+] Port {port} is OPEN")
+                open_ports.append(port)
+
+            sock.close()
+
+        except KeyboardInterrupt:
+            print(f"\n[!]Scan interrupted by user.")
+            break
+        
+        except socket.gaierror:
+            print("[!] Hostname could not be resolved.")
+            break
+
+        except socket.error:
+            print("[!] Could not connect to the server.")
+
+    print("\nScan complete.")
+    return open_ports
+
+
+if __name__ == "__main__":
+    target_host = input("Enter target IP or domain: ").strip()
+    scan_ports(target_host)
